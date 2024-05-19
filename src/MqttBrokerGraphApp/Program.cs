8fc92777e1fdbac19da1cd50aa772e7adf726c77
sandervandevelde <svelde@gmail.com>
2024-05-19 20:17:09 +0200

    Moved ARM logic to provider

--- src/MqttBrokerGraphApp/Program.cs
+++ src/MqttBrokerGraphApp/Program.cs
@@ -9,7 +9,7 @@ namespace MqttBrokerGraphApp
 {
 	internal class Program
 	{
-		static async Task Main(string[] args)
+		static void Main(string[] args)
 		{
 			Console.WriteLine("EventGrid MQTT broker - Client MQTT topics");
 			Console.WriteLine("==========================================");
@@ -21,122 +21,22 @@ namespace MqttBrokerGraphApp
 			// name of the eventgrid namespace
 			string? namespaceName = Environment.GetEnvironmentVariable("mqtt-graph-namespacename");
 
-			await GetClientTopics(subscriptionId, resourceGroupName, namespaceName);
-
-			await Task.Delay(1);
-		}
-
-		public static async Task GetClientTopics(string subscriptionId, string resourceGroupName, string namespaceName)
-		{
 			// Use host application access
 			//ArmClient client = new ArmClient(new DefaultAzureCredential());
 
 			// Use CLI access via AZ LOGIN 
-			var cred = new DefaultAzureCredential(new DefaultAzureCredentialOptions { ExcludeSharedTokenCacheCredential = true });
-			ArmClient client = new ArmClient(cred);
+			DefaultAzureCredential cred = new DefaultAzureCredential(new DefaultAzureCredentialOptions { ExcludeSharedTokenCacheCredential = true });
 
-			var eventGridNamespaceResourceId = EventGridNamespaceResource.CreateResourceIdentifier(subscriptionId, resourceGroupName, namespaceName);
-			EventGridNamespaceResource eventGridNamespace = client.GetEventGridNamespaceResource(eventGridNamespaceResourceId);
+			List<Client> clients = DeviceClientQueryProvider.GetClientTopics(subscriptionId, resourceGroupName, namespaceName, cred);
 
-			var collectionClients = eventGridNamespace.GetEventGridNamespaceClients();
-			var allClients = collectionClients.GetAll();
-			var listClients = allClients.ToList();
-
-			var collectionClientGroups = eventGridNamespace.GetEventGridNamespaceClientGroups();
-			var allClientGroups = collectionClientGroups.GetAll();
-			var listClientGroups = allClientGroups.ToList();
-
-			var collectionPermissionBindings = eventGridNamespace.GetEventGridNamespacePermissionBindings();
-			var allPermissionBindings = collectionPermissionBindings.GetAll();
-			var listPermissionBindings = allPermissionBindings.ToList();
-
-			var collectionTopicSpaces = eventGridNamespace.GetTopicSpaces();
-			var allTopicSpaces = collectionTopicSpaces.GetAll();
-			var listTopicSpaces = allTopicSpaces.ToList();
-
-			// all clients related to the query eg. " attributes.type IN ['audit'] "
-			foreach (var clientInList in listClients)
+			foreach (Client client in clients) 
 			{
-                await Console.Out.WriteLineAsync($"Client {clientInList.Data.Name}:");
+				Console.WriteLine($"Client {client.Name}");
 
-                var clientInGroups = new List<EventGridNamespaceClientGroupResource>();
-
-				var clientPublisherPermissingBindings = new List<EventGridNamespacePermissionBindingResource>();
-
-				var clientSubscriberPermissingBindings = new List<EventGridNamespacePermissionBindingResource>();
-
-				var clientPublisherTopicTemplates = new List<string>();
-
-				var clientSubscriberTopicTemplates = new List<string>();
-
-				foreach (var attribute in clientInList.Data.Attributes)
+				foreach (var topic in client.Topics)
 				{
-					foreach (var clientGroup in listClientGroups)
-					{
-						if ((clientGroup.Data.Query.ToLower() == "true") 
-								|| DeviceClientQueryHelper.Query(clientGroup.Data.Query, attribute))
-						{
-							if (!clientInGroups.Any(x => x.Id == clientGroup.Id))
-							{
-								clientInGroups.Add(clientGroup);
-							}
-						}
-					}
+					Console.WriteLine($"Can {topic.Usage} on: {topic.Name}");
 				}
-							
-				foreach(var permissionBinding in listPermissionBindings)
-				{
-					var clientGroup = clientInGroups.FirstOrDefault(x => x.Data.Name == permissionBinding.Data.ClientGroupName);
-
-					if (clientGroup != null)
-					{
-						if (permissionBinding.Data.Permission.Value == PermissionType.Subscriber)
-						{
-							if (!clientSubscriberPermissingBindings.Any(x => x.Id == permissionBinding.Id))
-							{
-								clientSubscriberPermissingBindings.Add(permissionBinding);
-							}
-						}
-
-						if (permissionBinding.Data.Permission.Value == PermissionType.Publisher)
-						{
-							if (!clientPublisherPermissingBindings.Any(x => x.Id == permissionBinding.Id))
-							{
-								clientPublisherPermissingBindings.Add(permissionBinding);
-							}
-						}
-					}
-				}
-
-				foreach (var clientPublisherPermissingBinding in clientPublisherPermissingBindings)
-				{
-					var topicSpace = listTopicSpaces.First(x => x.Data.Name == clientPublisherPermissingBinding.Data.TopicSpaceName);
-
-					clientPublisherTopicTemplates.AddRange(topicSpace.Data.TopicTemplates);
-				}
-
-				foreach (var clientSubscriberPermissingBinding in clientSubscriberPermissingBindings)
-				{
-					var topicSpace = listTopicSpaces.First(x => x.Data.Name == clientSubscriberPermissingBinding.Data.TopicSpaceName);
-
-					clientSubscriberTopicTemplates.AddRange(topicSpace.Data.TopicTemplates);
-				}
-
-				foreach(var x in clientPublisherTopicTemplates)
-				{
-					var y = x.Replace("${client.authenticationName}", clientInList.Data.Name);
-
-					await Console.Out.WriteLineAsync($"Can publish on: {y}");
-				}
-
-				foreach (var x in clientSubscriberTopicTemplates)
-				{
-					var y = x.Replace("${client.authenticationName}", clientInList.Data.Name);
-
-					await Console.Out.WriteLineAsync($"Can subscribe on: {y}");
-				}
-
-				await Console.Out.WriteLineAsync();
 			}
 		}
 	}
